77af146ee0f9b98f11f6262f22139be13092d967
Dashaun Sutton-Harris <114096734+dashaunn@users.noreply.github.com>
2022-10-12 09:43:08 -0500

    completed 2.5.1

---
+++ Chapter2Exercise/2.5.1/HelloWorld/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello stranger! What's your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("Howdy! Nice to meet you, " + name + ". :)");
+        }
+    }
+}
+
