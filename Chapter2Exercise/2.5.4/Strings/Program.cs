fb6fb01a1fe82cc96a76fbace8f687163e40a18c
Dashaun Sutton-Harris <114096734+dashaunn@users.noreply.github.com>
2022-10-12 13:03:02 -0500

    completed 2.5.4 part 1

---
+++ Chapter2Exercise/2.5.4/Strings/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sentence = "Alice was beginning to get very tired of sitting by her sister on the bank," +
+                " and of having nothing to do: once or twice she had peeped into the book her sister was reading," +
+                " but it had no pictures or conversations in it, 'and what is the use of a book,' thought Alice 'without pictures or conversation?'";
+            string input;
+            string searchTerm;
+            string searchSentence;
+            //string finalSentence;
+
+            Console.WriteLine(sentence + "\n");
+            Console.WriteLine("Please enter a word to search for from the sentence above.");
+            input = Console.ReadLine();
+            searchTerm = input.ToLower();
+            searchSentence = sentence.ToLower();
+
+
+            if (searchSentence.IndexOf(searchTerm, 0) != -1)
+            {
+                Console.WriteLine("true");
+                Console.WriteLine("Index of match: " + "[" + (searchSentence.IndexOf(searchTerm)) + "]");
+                Console.WriteLine("Length of match: " + (searchTerm.Length));
+                string finalSentence = sentence.Substring(3,sentence.Length);
+                Console.WriteLine(finalSentence);
+            } else
+            {
+                Console.WriteLine("false");
+            }
+           
+        }
+    }
+}
+
