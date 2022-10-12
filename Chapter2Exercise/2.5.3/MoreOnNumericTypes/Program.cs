58fb928a4da78d211121eb97be9605a30dd6b9ed
Dashaun Sutton-Harris <114096734+dashaunn@users.noreply.github.com>
2022-10-12 10:39:58 -0500

    completed 2.5.3

---
+++ Chapter2Exercise/2.5.3/MoreOnNumericTypes/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace MoreOnNumericTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string milesInput;
+            string gasInput;
+            int miles;
+            int gas;
+
+            Console.WriteLine("Jeez, it looks like you've been travelling for ages in that thing! How many miles was your trip?");
+            milesInput = Console.ReadLine();
+            miles = int.Parse(milesInput);
+            Console.WriteLine("How many gallons of gas did that cost you?");
+            gasInput = Console.ReadLine();
+            gas = int.Parse(gasInput);
+            Console.WriteLine("Really? That thing can do " + (miles/gas) + " miles per gallon!?");
+
+        }
+    }
+}
+
